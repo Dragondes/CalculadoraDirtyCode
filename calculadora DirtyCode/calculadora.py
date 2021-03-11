926c8249e8eadae86ce177d3d93beca51a7f3699
Carlos <60761459+Dragondes@users.noreply.github.com>
2021-03-11 19:19:02 -0400

    Add files via upload

---
+++ calculadora DirtyCode/calculadora.py
@@ -0,0 +1,24 @@
+def Calculadora():
+    print("""Menu
+    1) Suma
+    2) Resta
+    3) Multiplicacion
+    4) Division
+    5) Salir""")
+    var = int(input("Selecione Opcion\n"))
+    while (var >0 and var <5):
+        x = int(input("Ingrese Numero\n"))
+        y = int(input("Ingrese Otro Numero\n"))
+        if (var==1):
+            print("La Suma es:", x+y)
+            var = int(input("Selecione Opcion\n"))
+        elif(var==2):
+            print("La Resta es:",x-y)
+            var = int(input("Selecione Opcion\n"))
+        elif(var==3):
+            print("La Multiplicacion es:",x*y)
+            var = int(input("Selecione Opcion\n"))
+        elif(var==4):
+            print("La Division es:", x/y)
+            var = int(input("Selecione Opcion\n"))
+Calculadora()
