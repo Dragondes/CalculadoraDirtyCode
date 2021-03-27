a647d44c4c856710510116c737c83ac017c9a00a
Andrew <andrew.j.t.p@gmail.com>
2021-03-27 18:43:50 -0400

    clean-code-calculadora

--- calculadora DirtyCode/calculadora.py
+++ calculadora DirtyCode/calculadora.py
@@ -1,27 +1,46 @@
-def Calculadora():
+def menu():
     print("""Menu
     1) Suma
     2) Resta
     3) Multiplicacion
     4) Division
     5) Salir""")
-    var = int(input("Selecione Opcion\n"))
-    while (var >0 and var <5):
-        x = int(input("Ingrese Numero\n"))
-        y = int(input("Ingrese Otro Numero\n"))
-        if (var==1):
-            print("La Suma es:", x+y)
-            var = int(input("Selecione Opcion\n"))
-        elif(var==2):
-            print("La Resta es:",x-y)
-            var = int(input("Selecione Opcion\n"))
-        elif(var==3):
-            print("La Multiplicacion es:",x*y)
-            var = int(input("Selecione Opcion\n"))
-        elif(var==4):
-            print("La Division es:", x/y)
-            var = int(input("Selecione Opcion\n"))
+def suma(numero1,numero2):
+    reslutado = numero1 + numero2
+    return reslutado
 
-#este codigo esta siendo refactorizado
+def resta(numero1,numero2):
+    reslutado = numero1 - numero2
+    return reslutado
 
-Calculadora()
+def multiplicacion(numero1,numero2):
+    reslutado = numero1 * numero2
+    return reslutado
+def division(numero1,numero2):
+    reslutado = numero1 / numero2
+    return reslutado
+
+def calculadora():
+    menu() #imprime menu
+    opcion=int(input("Selecione Opcion\n"))
+    
+    while (opcion >0 and opcion <5):
+        numero1 = int(input("Ingrese Numero\n"))    #primer numero para la operacion
+        numero2 = int(input("Ingrese Otro Numero\n")) #segundo numero para la operacion
+        
+        if (opcion==1):      #elige la opcion para la operacion
+            print("La Suma es:", suma(numero1,numero2))
+            
+        elif(opcion==2):
+            print("La Resta es:",resta(numero1,numero2))
+            
+        elif(opcion==3):
+            print("La Multiplicacion es:",multiplicacion(numero1,numero2))
+            
+        elif(opcion==4):
+            print("La Division es:", division(numero1,numero2))            
+
+        menu()
+        opcion=int(input("Selecione Opcion\n"))       
+
+calculadora()
